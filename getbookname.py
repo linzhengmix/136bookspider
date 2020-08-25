ade14ff3a8fe5bfe26ce1efa29b820e5b0f32085
Lynzheng <RinLinux@users.noreply.github.com>
2020-08-25 16:37:49 +0800

    modified *.py

--- getbookname.py
+++ getbookname.py
@@ -5,15 +5,20 @@ from download_class import download_story
 from bs4 import BeautifulSoup
 
 
-if __name__ == '__main__':
-    url = 'http://www.136book.com/wanjie.php'
+def getbookurl(url):
+
     headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) \
             Chrome/18.0.1025.166  Safari/535.19'}
     response = requests.get(url=url,headers=headers,verify=False)
     soup = BeautifulSoup(response.text, 'lxml')
     soup_texts = soup.find_all('a',target="_blank")
+    urls = []
     for i in soup_texts:
-        # print(i.text)
-        url = i.get('href') #get the urls
+        url = i.get('href')
+        urls.append(url)
         print(url)
-        # download_story(url)
+    return urls
+
+if __name__ == '__main__':
+    url = 'http://www.136book.com/wanjie.php'
+    getbookurl(url=url)
